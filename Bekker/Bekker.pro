c721a577324226c517ce9da26b9fbfbf45f718ec
DenisVadimovich <pushkodenis@gmail.com>
2022-12-22 13:37:24 +0300

    Start Prog. Add Auth in prog

---
+++ Bekker/Bekker.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2022-12-22T12:04:11
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = Bekker
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ Bekker/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int Choice,Choice1;
+    setlocale(LC_ALL, "rus");
+    cout << "1. Auth" << endl;
+    cin >> Choice;
+    switch (Choice) {
+
+    case 1:
+        system("cls");
+        string Login,Password;
+        cout << "Login:" << endl;
+        cin >> Login;
+        cout << "Password" << endl;
+        cin >> Password;
+        if (Login == "Casir" & Password == "123"){
+            system("cls");
+            cout << "1. Start selling" << endl;
+            cout << "2. Stock balance" << endl;
+            cout << "3. ..." << endl;
+            cout << "4. ..." << endl;
+            cout << "5. ..." << endl;
+            cout << "6. Quit" << endl;
+            cout << "Choice: ";
+            cin >> Choice1;
+            switch (Choice1) {
+                case 6:
+                break;
+}
+        }
+    }
+    return(0);
+}
